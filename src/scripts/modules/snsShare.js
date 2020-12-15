1787c8e268305f0f3c6d5c88fb47d25d03fb7aa7
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2020-12-15 16:46:10 +0700

    fix: save fix

--- src/scripts/modules/snsShare.js
+++ src/scripts/modules/snsShare.js
@@ -4,9 +4,10 @@ import { indexOfPredictRamdom } from './predict'
 const facebookShare = () => {
   const btnShareFbElm = document.getElementById('bthFacebookShare')
   // eslint-disable-next-line no-undef
-  // const urlMain = 'https://krungthaihny2021.com/'
-  // const urlShare = encodeURIComponent(`${urlMain}share_${indexOfPredictRamdom}.html`)
-  btnShareFbElm.setAttribute('href', 'https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer')
+  const appId = '145634995501895'
+  const urlMain = 'https://krungthaihny2021.com/'
+  const urlShare = encodeURIComponent(`${urlMain}share_${indexOfPredictRamdom}.html`)
+  btnShareFbElm.setAttribute('href', `https://www.facebook.com/dialog/share?app_id=${appId}&display=popup&href=${encodeURIComponent(urlShare)}&redirect_uri=${encodeURIComponent(urlMain)}`)
   // window.open(`https://www.facebook.com/sharer/sharer.php?u=${urlShare}&hashtag=%23KrungthaiHNY2021`, 'fbShareWindow')
 }
 
